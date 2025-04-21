d2027aa153429587f7b8ab6bcfcc0a258aca7037
pcb3 <holypallyxiii@outlook.com>
2025-04-21 21:36:13 +0700

    create PlayerData resource and player scene/scipt

---
+++ scripts/PlayerData.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name PlayerData
+
+@export var max_speed: float = 100.0
+@export var acceleration: float = 500.0
+@export var jump_force: float = 200.0
+@export var gravity: float = 980.0
---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var player_data =preload("res://resources/PlayerData.tres")
+
+# State machine
+enum State { IDLE, RUNNING, JUMPING, FALLING }
+var state : State = State.IDLE
+
+func _physics_process(delta: float) -> void:
+	# Get horizontal input
+	var input_x: float = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	# State machine
+	match state:
+		State.IDLE:
+			# Transition to RUNNING if moving
+			if input_x != 0:
+				state = State.RUNNING
+			# Transition to JUMPING if jumping
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = -player_data.jump_force
+				state = State.JUMPING
+			# Apply gravity
+			velocity.y += player_data.gravity * delta
+			# Decelerate on ground
+			velocity.x = move_toward(velocity.x, 0, player_data.acceleration * delta)
+		
+		State.RUNNING:
+			# Transition to IDLE if no input
+			if input_x == 0:
+				state = State.IDLE
+			# Transition to JUMPING if jumping
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = -player_data.jump_force
+				state = State.JUMPING
+			# Move horizontally
+			velocity.x = move_toward(velocity.x, input_x * player_data.max_speed, player_data.acceleration * delta)
+			# Apply gravity
+			velocity.y += player_data.gravity * delta
